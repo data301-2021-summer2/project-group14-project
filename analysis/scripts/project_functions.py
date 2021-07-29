b2104f3bdb7a09f5799ff5d6d07326d67de77474
satveersangha <satveersangha3@gmail.com>
2021-07-29 15:37:38 -0700

    updated project_functions file

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,39 +1,6 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a7411b29-8196-4359-b5c2-74f6800c3519",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
-
 import seaborn as sns
 import pandas as pd
+path = 'data/raw/waterpotability.csv'
 
 def load_process(path):
     df = (
